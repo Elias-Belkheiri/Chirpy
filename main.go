8c1929208db90a65ed37227e6bf0dd4d9f94cf12
Elias <ilyasseyasser3@gmail.com>
2024-04-30 11:24:30 +0100

    added a counter

--- main.go
+++ main.go
@@ -7,11 +7,39 @@ import (
 	// "net"
 	// "html"
 	"net/http"
+	"sync"
+	"strconv"
 )
+
+type apiConfig struct {
+	mu				sync.Mutex
+	fileserverHits 	int
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.mu.Lock()
+		fmt.Println("Incrementing...")
+		cfg.fileserverHits++
+		cfg.mu.Unlock()
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) middlewareGetMetrics() int {
+	// cfg.mu.Lock()
+	// defer cfg.mu.Unlock()
+	return cfg.fileserverHits
+}
+
+func (cfg *apiConfig) middlewareResetMetrics() {
+	cfg.mu.Lock()
+	defer cfg.mu.Unlock()
+	cfg.fileserverHits = 0
+}
 
 func middlewareCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Mid1")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
 		w.Header().Set("Access-Control-Allow-Headers", "*")
@@ -24,15 +52,26 @@ func middlewareCors(next http.Handler) http.Handler {
 }
 
 func main() {
+	counter := apiConfig{sync.Mutex{}, 0}
+
 	mux := http.NewServeMux()
 	corsMux := middlewareCors(http.FileServer(http.Dir("./")))	
-	mux.Handle("/app/*", corsMux)
-	mux.Handle("/healthz", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+	mux.Handle("/app/*", counter.middlewareMetricsInc(corsMux))
+	mux.Handle("/healthz", counter.middlewareMetricsInc(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
+	})))
+	mux.Handle("/count", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		count := "count: " + strconv.Itoa(counter.middlewareGetMetrics()) 
+		fmt.Println(count)
+		w.Write([]byte(count))
 	}))
-	// http.HandleFunc("/greeting", greeting)
+	mux.Handle("/reset", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		counter.middlewareResetMetrics()
+		fmt.Println("Counter has been reset")
+	}))
 	
 	log.Println("Starting server....")
 	http.ListenAndServe(":1224", mux)
